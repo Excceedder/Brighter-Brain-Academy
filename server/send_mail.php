fab31fe9198d5637a5c57c060a418c02bbb60df5
Excel Oghenetejiri Prosper <117411603+Excceedder@users.noreply.github.com>
2023-03-24 08:57:09 +0100

    Initial commit

--- server/send_mail.php
+++ server/send_mail.php
@@ -3,12 +3,12 @@
 function send_mail($mail_data)
 {
     include "mail_server.php";
-    $template_file = './server/templates/verify.php';
+    $template_file = '../server/templates/verify.php';
     $swap_var = array(
         "{ORGANISATION}" => "Brighter Brain Academy",
         "{FULL_NAMES}" => $mail_data['full_names'],
         "{ROLE}" => $mail_data['account_role'],
-        "{URL}" => "https://manager.brighterbrainacademy.com/verify?designated_manager_id={$mail_data['designated_manager_id']}",
+        "{URL}" => "https://brighterbrainacademy.com/manager/verify?designated_manager_id={$mail_data['designated_manager_id']}",
     );
 
     $message = file_get_contents($template_file);
