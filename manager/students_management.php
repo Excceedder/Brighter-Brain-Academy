c200d8dc8efd1c1e9319f241d902577858c3d05e
Excel Oghenetejiri Prosper <117411603+Excceedder@users.noreply.github.com>
2023-03-25 10:43:28 +0100

    Initial commit

--- manager/students_management.php
+++ manager/students_management.php
@@ -51,6 +51,15 @@
                     </div>
                     <!-- end page title -->
 
+                    <div class="row">
+                        <div class="col-auto">
+                            <div class="card" style="border: 1px dashed #343a40;">
+                                <div class="card-body p-2">
+                                    <img src="<?php echo $designated_student_data["profile_photo"] ?>" class="avatar avatar-lg rounded" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <?php
                     if (isset($_SESSION['feedback'])) {
                         $type = $_SESSION['type'];
@@ -88,15 +97,6 @@
                             return false;
                         }
                     ?>
-                        <div class="row">
-                            <div class="col-auto">
-                                <div class="card" style="border: 1px dashed #343a40;">
-                                    <div class="card-body p-2">
-                                        <img src="<?php echo $designated_student_data["profile_photo"] ?>" class="avatar avatar-lg rounded" alt="">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                         <div class="row">
                             <div class="col-12">
                                 <div class="card" style="border: 1px dashed #343a40;">
