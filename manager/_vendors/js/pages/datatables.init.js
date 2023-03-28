c9c8a9f59a7d795725d20d2df96f27f19767181d
Excel Oghenetejiri Prosper <117411603+Excceedder@users.noreply.github.com>
2023-03-28 11:23:13 +0100

    Initial commit

--- manager/_vendors/js/pages/datatables.init.js
+++ manager/_vendors/js/pages/datatables.init.js
@@ -10,17 +10,10 @@ $(document).ready(function () {
             className: "btn btn-success",
           },
         ],
-        dom:
-          '<"row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>' +
-          '<"row"<"col-sm-12"tr>>' +
-          '<"row"<"col-sm-12 col-md-5"i><"col-sm-12 col-md-7"p>>',
       })
       .buttons()
       .container()
       .appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)");
 
   $(".dataTables_length select").addClass("form-select form-select-sm");
-  $(".dataTables_filter input").addClass(
-    "form-control form-control-sm bg-success text-white"
-  );
 });
