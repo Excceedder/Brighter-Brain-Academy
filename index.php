d53bd7518de33fcf695d9571805e616c25b3d461
Excel Oghenetejiri Prosper <117411603+Excceedder@users.noreply.github.com>
2023-03-31 00:04:15 +0100

    Initial commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <!-- Meta Data -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>EduBlink | Online Education Platform</title>
+    <title>Brighter Brain Academy | Real Education Breeds Value</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
