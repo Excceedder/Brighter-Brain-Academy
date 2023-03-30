e3b13a3ee839dac9bc747791eee3cba9569402b4
Excel Oghenetejiri Prosper <117411603+Excceedder@users.noreply.github.com>
2023-03-30 20:40:10 +0100

    Initial commit

--- termly_report/_vendors/js/main.js
+++ termly_report/_vendors/js/main.js
@@ -6,15 +6,23 @@
   ----------------------------------------------------------------*/
   $("#download_btn").on("click", function () {
     var downloadSection = $("#download_section")[0];
-    var cWidth = downloadSection.offsetWidth;
-    var cHeight = downloadSection.offsetHeight;
+    var cWidth = downloadSection.clientWidth;
+    var cHeight = downloadSection.clientHeight;
 
-    html2canvas(downloadSection, { allowTaint: true }).then(function (canvas) {
-      var imgData = canvas.toDataURL("image/jpeg", 1.0);
-      var link = document.createElement("a");
-      link.download = "termly_report.jpg";
-      link.href = imgData;
-      link.click();
-    });
+    var canvas = document.createElement("canvas");
+    canvas.width = cWidth;
+    canvas.height = cHeight;
+    var context = canvas.getContext("2d");
+
+    html2canvas(downloadSection, { allowTaint: true, canvas: canvas }).then(
+      function (canvas) {
+        context.drawImage(canvas, 0, 0, cWidth, cHeight);
+        var imgData = canvas.toDataURL("image/jpeg", 1.0);
+        var link = document.createElement("a");
+        link.download = "termly_report.jpg";
+        link.href = imgData;
+        link.click();
+      }
+    );
   });
 })(jQuery); // End of use strict
