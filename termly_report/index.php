5d4cf243c3558e8714e60390a99d95d79a2a4aef
Excel Oghenetejiri Prosper <117411603+Excceedder@users.noreply.github.com>
2023-03-30 17:51:27 +0100

    Initial commit

--- termly_report/index.php
+++ termly_report/index.php
@@ -39,7 +39,7 @@ if (isset($_SESSION['termly_report_id'])) {
         <span>Search New Report</span>
       </a>
     </div>
-    <div class="cs-invoice cs-style1" style="background-image: url('./_vendors/img/background.jpg'); background-repeat: no-repeat; background-size: cover;">
+    <div class="cs-invoice cs-style1" style="background-image: url('./_vendors/img/background.png'); background-repeat: no-repeat; background-size: cover;">
       <div class="cs-invoice_in" id="download_section">
         <div class="cs-invoice_head cs-type1 cs-mb25">
           <div class="cs-invoice_left">
@@ -1441,22 +1441,13 @@ if (isset($_SESSION['termly_report_id'])) {
         </div><!-- .cs-note -->
       </div>
       <div class="cs-invoice_btns cs-hide_print">
-        <a href="javascript:window.print()" class="cs-invoice_btn cs-color1">
-          <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512">
-            <path d="M384 368h24a40.12 40.12 0 0040-40V168a40.12 40.12 0 00-40-40H104a40.12 40.12 0 00-40 40v160a40.12 40.12 0 0040 40h24" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32" />
-            <rect x="128" y="240" width="256" height="208" rx="24.32" ry="24.32" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32" />
-            <path d="M384 128v-24a40.12 40.12 0 00-40-40H168a40.12 40.12 0 00-40 40v24" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32" />
-            <circle cx="392" cy="184" r="24" />
-          </svg>
-          <span>Print</span>
-        </a>
-        <button id="download_btn" class="cs-invoice_btn cs-color2">
+        <button id="download_btn" class="cs-invoice_btn cs-color2" style="border-radius: 8px;">
           <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512">
             <title>Download</title>
             <path d="M336 176h40a40 40 0 0140 40v208a40 40 0 01-40 40H136a40 40 0 01-40-40V216a40 40 0 0140-40h40" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" />
             <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M176 272l80 80 80-80M256 48v288" />
           </svg>
-          <span>Download</span>
+          <span>Download Termly Report</span>
         </button>
       </div>
     </div>
