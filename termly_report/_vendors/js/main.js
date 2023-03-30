746d56528d5f1b0ccea0f03835e708506f69445c
Excel Oghenetejiri Prosper <117411603+Excceedder@users.noreply.github.com>
2023-03-30 20:31:33 +0100

    Initial commit

--- termly_report/_vendors/js/main.js
+++ termly_report/_vendors/js/main.js
@@ -1,20 +1,20 @@
-// (function ($) {
-//   "use strict";
+(function ($) {
+  "use strict";
 
-//   /*--------------------------------------------------------------
-//   ## Down Load Button Function
-//   ----------------------------------------------------------------*/
-//   $("#download_btn").on("click", function () {
-//     var downloadSection = $("#download_section")[0];
-//     var cWidth = downloadSection.offsetWidth;
-//     var cHeight = downloadSection.offsetHeight;
+  /*--------------------------------------------------------------
+  ## Down Load Button Function
+  ----------------------------------------------------------------*/
+  $("#download_btn").on("click", function () {
+    var downloadSection = $("#download_section")[0];
+    var cWidth = downloadSection.offsetWidth;
+    var cHeight = downloadSection.offsetHeight;
 
-//     html2canvas(downloadSection, { allowTaint: true }).then(function (canvas) {
-//       var imgData = canvas.toDataURL("image/jpeg", 1.0);
-//       var link = document.createElement("a");
-//       link.download = "termly_report.jpg";
-//       link.href = imgData;
-//       link.click();
-//     });
-//   });
-// })(jQuery); // End of use strict
+    html2canvas(downloadSection, { allowTaint: true }).then(function (canvas) {
+      var imgData = canvas.toDataURL("image/jpeg", 1.0);
+      var link = document.createElement("a");
+      link.download = "termly_report.jpg";
+      link.href = imgData;
+      link.click();
+    });
+  });
+})(jQuery); // End of use strict
