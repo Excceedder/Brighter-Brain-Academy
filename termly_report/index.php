c4b45e31c25aa2093ecfbaa7113f180b2c0acb96
Excel Oghenetejiri Prosper <117411603+Excceedder@users.noreply.github.com>
2023-04-01 14:36:01 +0100

    Initial commit

--- termly_report/index.php
+++ termly_report/index.php
@@ -1713,18 +1713,6 @@ if (isset($_SESSION['termly_report_id'])) {
           </div>
         </div>
 
-        <div class="cs-note">
-          <div class="cs-note_left">
-            <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512">
-              <path d="M416 221.25V416a48 48 0 01-48 48H144a48 48 0 01-48-48V96a48 48 0 0148-48h98.75a32 32 0 0122.62 9.37l141.26 141.26a32 32 0 019.37 22.62z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32" />
-              <path d="M256 56v120a32 32 0 0032 32h120M176 288h160M176 368h160" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" />
-            </svg>
-          </div>
-          <div class="cs-note_right">
-            <p class="cs-mb0"><b class="cs-primary_color cs-bold">Note:</b></p>
-            <p class="cs-m0">Here we can write a additional notes for the client to get a better understanding of this invoice.</p>
-          </div>
-        </div><!-- .cs-note -->
       </div>
     </div>
   </div>
