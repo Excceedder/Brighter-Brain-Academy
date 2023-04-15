745308cf66908cde5403f80ec559a3536eb3aa5b
Excel Oghenetejiri Prosper <117411603+Excceedder@users.noreply.github.com>
2023-04-15 21:49:18 +0100

    Initial commit

--- index.php
+++ index.php
@@ -62,119 +62,31 @@
                         <div class="header-mainnav">
                             <nav class="mainmenu-nav">
                                 <ul class="mainmenu">
-                                    <li class="has-droupdown"><a href="#">Home</a>
-                                        <ul class="mega-menu mega-menu-one">
-                                            <li>
-                                                <ul class="submenu mega-sub-menu mega-sub-menu-01">
-                                                    <li><a href="index.html">EduBlink Education <span class="badge-1">hot</span></a></li>
-                                                    <li><a href="index-distant-learning.html">Distant Learning</a></li>
-                                                    <li><a href="index-university.html">University</a></li>
-                                                    <li><a href="index-online-academy.html">Online Academy <span class="badge-1">hot</span></a></li>
-                                                    <li><a href="index-modern-schooling.html">Modern Schooling</a></li>
-                                                    <li><a href="index-kitchen.html">Kitchen Coach</a></li>
-                                                    <li><a href="index-yoga.html">Yoga Instructor</a></li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <ul class="submenu mega-sub-menu mega-sub-menu-01">
-                                                    <li><a href="index-kindergarten.html">Kindergarten</a></li>
-                                                    <li><a href="index-health-coach.html">Health Coch <span class="badge">new</span></a></li>
-                                                    <li><a href="index-language-academy.html">Language Academy <span class="badge">new</span></a></li>
-                                                    <li><a href="index-remote-training.html">Remote Training <span class="badge">new</span></a></li>
-                                                    <li><a href="index-photography.html">Photography <span class="badge">new</span></a></li>
-                                                    <li><a href="https://edublink.html.dark.devsblink.com/" target="_blank">Dark Version</a></li>
-                                                    <li><a href="index-landing.html">Landing Demo</a></li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <ul class="submenu mega-sub-menu-01">
-                                                    <li>
-                                                        <a href="https://1.envato.market/5bQ022">
-                                                            <img src="_vendors/images/others/mega-menu-image.webp" alt="advertising Image">
-                                                        </a>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li class="has-droupdown"><a href="#">Pages</a>
-                                        <ul class="mega-menu">
-                                            <li>
-                                                <h6 class="menu-title">Inner Pages</h6>
-                                                <ul class="submenu mega-sub-menu-01">
-                                                    <li><a href="about-one.html">About Us 1</a></li>
-                                                    <li><a href="about-two.html">About Us 2</a></li>
-                                                    <li><a href="about-three.html">About Us 3</a></li>
-                                                    <li><a href="team-one.html">Instructor 1</a></li>
-                                                    <li><a href="team-two.html">Instructor 2</a></li>
-                                                    <li><a href="team-three.html">Instructor 3</a></li>
-                                                    <li><a href="team-details.html">Instructor Profile</a></li>
-                                                    <li><a href="faq.html">Faq's</a></li>
-                                                    <li><a href="404.html">404 Error</a></li>
-                                                    <li><a href="coming-soon.html">Coming Soon</a></li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <h6 class="menu-title">Inner Pages</h6>
-                                                <ul class="submenu mega-sub-menu-01">
-                                                    <li><a href="gallery-grid.html">Gallery Grid</a></li>
-                                                    <li><a href="gallery-masonry.html">Gallery Masonry</a></li>
-                                                    <li><a href="event-grid.html">Event Grid</a></li>
-                                                    <li><a href="event-list.html">Event List</a></li>
-                                                    <li><a href="event-details.html">Event Details</a></li>
-                                                    <li><a href="pricing-table.html">Pricing Table</a></li>
-                                                    <li><a href="purchase-guide.html">Purchase Guide</a></li>
-                                                    <li><a href="privacy-policy.html">Privacy Policy</a></li>
-                                                    <li><a href="terms-condition.html">Terms & Condition</a></li>
-                                                    <li><a href="my-account.html">Sign In</a></li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <h6 class="menu-title">Shop Pages</h6>
-                                                <ul class="submenu mega-sub-menu-01">
-                                                    <li><a href="shop.html">Shop</a></li>
-                                                    <li><a href="product-details.html">Product Details</a></li>
-                                                    <li><a href="cart.html">Cart</a></li>
-                                                    <li><a href="wishlist.html">Wishlist</a></li>
-                                                    <li><a href="checkout.html">Checkout</a></li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
+                                    <li><a href="./">Home</a> </li>
 
-                                    <li class="has-droupdown"><a href="#">Courses</a>
+                                    <li><a href="./">Why Choose Us</a> </li>
+
+                                    <li class="has-droupdown"><a href="#">Academics</a>
                                         <ul class="submenu">
                                             <li><a href="course-one.html">Course Style 1</a></li>
                                             <li><a href="course-two.html">Course Style 2</a></li>
                                             <li><a href="course-three.html">Course Style 3</a></li>
                                             <li><a href="course-four.html">Course Style 4</a></li>
-                                            <li><a href="course-five.html">Course Style 5</a></li>
-                                            <li><a href="course-details.html">Course Details 1</a></li>
-                                            <li><a href="course-details-2.html">Course Details 2</a></li>
-                                            <li><a href="course-details-3.html">Course Details 3</a></li>
                                         </ul>
                                     </li>
 
-                                    <li class="has-droupdown"><a href="#">Blog</a>
+                                    <li class="has-droupdown"><a href="#">Tutorials</a>
                                         <ul class="submenu">
-                                            <li><a href="blog-standard.html">Blog Standard</a></li>
-                                            <li><a href="blog-masonry.html">Blog Masonry</a></li>
-                                            <li><a href="blog-list.html">Blog List</a></li>
-                                            <li><a href="blog-details.html">Blog Details</a></li>
+                                            <li><a href="course-one.html">JAMB</a></li>
+                                            <li><a href="course-two.html">SSCE</a></li>
+                                            <li><a href="course-three.html">POST UTME</a></li>
                                         </ul>
                                     </li>
-                                    <li class="has-droupdown"><a href="#">Contact</a>
-                                        <ul class="submenu">
-                                            <li><a href="contact-us.html">Contact Us</a></li>
-                                            <li><a href="contact-me.html">Contact Me</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="has-droupdown"><a href="#">Contact</a>
-                                        <ul class="submenu">
-                                            <li><a href="contact-us.html">Contact Us</a></li>
-                                            <li><a href="contact-me.html">Contact Me</a></li>
-                                        </ul>
-                                    </li>
+                                    
+                                    <li><a href="./">Public Info</a> </li>
+
+                                    <li><a href="./">Contact Us</a></li>
+                                    
                                 </ul>
                             </nav>
                         </div>
