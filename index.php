4c3925d733a76a7e34973174b878c556ccbaa51b
Excel Oghenetejiri Prosper <117411603+Excceedder@users.noreply.github.com>
2023-05-31 00:34:15 +0100

    Initial commit

--- index.php
+++ index.php
@@ -35,10 +35,6 @@
             <div class="preloader-spin-1"></div>
             <div class="preloader-spin-2"></div>
         </div>
-        <div class="preloader-close-btn-wraper">
-            <span class="btn btn-primary preloader-close-btn">
-                Cancel Preloader</span>
-        </div>
     </div>
 
     <div id="main-wrapper" class="main-wrapper">
