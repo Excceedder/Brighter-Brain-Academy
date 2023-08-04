d67e29b3ad7a57ad1811d54b9075513145cd84b5
Excceedder <117411603+Excceedder@users.noreply.github.com>
2023-08-04 15:04:30 +0100

    Initial commit

--- termly_report/index.php
+++ termly_report/index.php
@@ -27,6 +27,7 @@ if (isset($_SESSION['termly_report_id'])) {
   <link rel="shortcut icon" type="image/x-icon" href="_vendors/img/logo.png">
   <link rel="stylesheet" href="_vendors/css/style.css">
   <script src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
   <style>
     .bg-img-2 {
